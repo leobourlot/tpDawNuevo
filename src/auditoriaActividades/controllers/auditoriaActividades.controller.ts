65d64da8e724b4765bb5f93ed89f406736a79950
leobourlot <leobourlot@gmail.com>
2024-04-28 13:09:15 -0300

    algunos metodos en auditoria

--- src/auditoriaActividades/controllers/auditoriaActividades.controller.ts
+++ src/auditoriaActividades/controllers/auditoriaActividades.controller.ts
@@ -4,6 +4,7 @@ import { AuthModule } from "src/auth/auth.module";
 import { ApiBearerAuth } from "@nestjs/swagger";
 import { Roles } from "src/auth/decorators/roles.decorator";
 import { RolesEnum } from "src/auth/enums/roles-enum";
+import { Usuario } from "src/auth/entities/usuario.entity";
 
 @Controller('/auditoriaActividades')
 export class AuditoriaActividadesController{
@@ -18,6 +19,30 @@ export class AuditoriaActividadesController{
     @Get()
     async obtenerAuditoriasActividades(){
         return await this.auditoriaActividadesService.obtenerAuditoriasActividades();
+    }
+
+    @UseGuards(AuthModule)
+    @ApiBearerAuth()
+    @Roles([RolesEnum.ADMINISTRADOR])
+    @Get('/actividadesModificadas')
+    async obtenerModificaciones(){
+        return await this.auditoriaActividadesService.obtenerModificaciones();
+    }
+
+    @UseGuards(AuthModule)
+    @ApiBearerAuth()
+    @Roles([RolesEnum.ADMINISTRADOR])
+    @Get('/actividadesFinalizadas')
+    async obtenerFinalizadas(){
+        return await this.auditoriaActividadesService.obtenerFinalizadas();
+    }
+    
+    @UseGuards(AuthModule)
+    @ApiBearerAuth()
+    @Roles([RolesEnum.ADMINISTRADOR])
+    @Get('/actividadesUsuario/:id')
+    async obtenerActividadesUsuario(idUsuarioModificacion: Usuario){
+        return await this.auditoriaActividadesService.obtenerActividadesUsuario(idUsuarioModificacion);
     }
         
 
--- src/auditoriaActividades/services/auditoriaActividades.services.ts
+++ src/auditoriaActividades/services/auditoriaActividades.services.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@nestjs/common";
 import { AuditoriaActividades } from "../entities/auditoriaActividades.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { EstadosActividadEnum } from "src/actividades/enums/estadoActividad.enum";
+import { Usuario } from "src/auth/entities/usuario.entity";
 
 
 @Injectable()
@@ -16,5 +18,46 @@ export class AuditoriaActividadesService{
 
         return auditoriasActividades
     }
+
+    async obtenerModificaciones(): Promise<AuditoriaActividades[]>{
+        const actividadesModificadas: AuditoriaActividades[] = await this.auditoriaActividadesRepo.find({
+            where: {
+                operacion: "MODIFICACION"
+            },
+        });
+        return actividadesModificadas    
+        
+    }
+
+    async obtenerFinalizadas(): Promise<AuditoriaActividades[]>{
+        const actividadesFinalizadas: AuditoriaActividades[] = await this.auditoriaActividadesRepo.find({
+            where: {
+                estado: EstadosActividadEnum.FINALIZADO
+            },
+        });
+        return actividadesFinalizadas    
+        
+    }
+
+    // async obtenerActividadesUsuario(idUsuario: number): Promise<AuditoriaActividades[]>{
+    //     const opciones: FindOneOptions<Actividad> = { where: { idActividad: id } };
+        
+    //     const actividadesUsuario: AuditoriaActividades[] = await this.auditoriaActividadesRepo.find({
+    //         where: {
+    //             idUsuarioModificacion: idUsuario 
+    //         },
+    //     });
+    //     return actividadesUsuario    
+        
+    // }
+
+    async obtenerActividadesUsuario(usuario: Usuario): Promise<AuditoriaActividades[]> {   // vvvvvvvveeeeeeeeeerrrrrrrrrr, devuelve todas y no solo la de un usuario
+
+        const consulta = this.auditoriaActividadesRepo
+            .createQueryBuilder('actividadesAuditoria')
+            .innerJoin('actividadesAuditoria.idUsuarioModificacion', 'usuario');
+        return await consulta.getMany();
+    }
+
 
 }
