58a30af78c2774b42ee9672b45c0a1081a60920f
Ariel <ariel43859@gmail.com>
2024-05-18 15:14:23 -0300

    Base del proyecto front

---
+++ frontend/src/app/componentes/login/login.component.ts
@@ -0,0 +1,13 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+    standalone:true,
+    templateUrl:'../login/login.component.html',
+    styleUrl:'../login/login.component.scss',
+    selector:'app-login',
+    imports:[],
+})
+export class LoginComponent{
+
+}
