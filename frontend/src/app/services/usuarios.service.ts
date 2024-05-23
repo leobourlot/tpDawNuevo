0ef7e52faf5b781a19cc4a72ba60505e38c874f3
leobourlot <leobourlot@gmail.com>
2024-05-23 00:03:32 -0300

    .

--- frontend/src/app/services/usuarios.service.ts
+++ frontend/src/app/services/usuarios.service.ts
@@ -5,13 +5,13 @@ import { environment } from '../environments/environment';
 import { UsuarioDto } from '../dtos/usuario.dto';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class UsuariosService {
-  constructor(private client: HttpClient) {}
+    constructor(private client: HttpClient) { }
 
-  getUsuarios(): Observable<UsuarioDto[]> {
-    console.log(this.client.get<UsuarioDto[]>(environment?.apiUrl + '/usuarios'));
-    return this.client.get<UsuarioDto[]>(environment?.apiUrl + '/usuarios');
-  }
+    getUsuarios(): Observable<UsuarioDto[]> {
+        console.log(this.client.get<UsuarioDto[]>(environment?.apiUrl + '/usuarios'));
+        return this.client.get<UsuarioDto[]>(environment?.apiUrl + '/usuarios');
+    }
 }
