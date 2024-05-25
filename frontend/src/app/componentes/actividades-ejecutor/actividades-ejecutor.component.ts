b8463d77b1f609c128d336ae6e7832a787e7a752
auroraarguet@gmail.com <auroraarguet@gmail.com>
2024-05-25 15:55:07 -0300

    tabla ejecutor cambios

--- frontend/src/app/componentes/actividades-ejecutor/actividades-ejecutor.component.ts
+++ frontend/src/app/componentes/actividades-ejecutor/actividades-ejecutor.component.ts
@@ -1,41 +1,109 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ButtonModule } from 'primeng/button';
-import { HttpClientModule } from '@angular/common/http';
-import { Actividad } from '../../model/actividad.model';
-import { BaseComponent } from '../base/base.component';
-import { OperacionActividadEnum } from '../../enums/operacion-actividad.enum'; // Importa el enum aquí
+import { Component, ViewChild } from '@angular/core';
+//import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { Table, TableModule } from 'primeng/table';
+import { ActividadDto } from '../../dtos/actividad.dto';
 import { ActividadesService } from '../../services/actividades.service';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+import { ActividadDialogComponent } from '../actividad-dialog/actividad-dialog.component';
+import { NgFor, NgIf } from '@angular/common';
+import { MessageService, SelectItem } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { Router, RouterModule } from '@angular/router';
+import { TablaBaseComponent } from '../tabla-base/tabla-base.component';
+import { BaseComponent } from '../base/base.component';
+import { AuditoriaActividadDto } from '../../dtos/auditoria-actividades.dto';
+import { AuditoriaActividadesService } from '../../services/auditoriaactividades.services';
 
+/**
+ * Pantalla para los usuarios con el rol de EJECUTOR
+ */
 @Component({
   selector: 'app-actividades-ejecutor',
   standalone: true,
-  imports: [CommonModule, ButtonModule, HttpClientModule, BaseComponent],
+  imports: [
+    ActividadDialogComponent,
+    ButtonModule,
+    TooltipModule,
+    ToastModule,
+    NgIf,
+    NgFor,
+    RouterModule,
+    TablaBaseComponent,
+    TableModule,
+    BaseComponent
+  ],
   templateUrl: './actividades-ejecutor.component.html',
-  styleUrls: ['./actividades-ejecutor.component.scss']
+  styleUrl: './actividades-ejecutor.component.scss',
 })
-export class ActividadesEjecutorComponent implements OnInit {
-  actividades: Actividad[] = [];
-  operacionEnum = OperacionActividadEnum; 
+export class ActividadesEjecutorComponent {
+  actividades!: ActividadDto[];
+  auditoriaActividades!: AuditoriaActividadDto[];
+  dialogVisible: boolean = false;
+  accion!: string;
+  actividadSeleccionada!: ActividadDto | null;
+  columnas!: { field: string; header: string; filter?: boolean }[];
+  opcionesDeFiltro!: SelectItem[];
 
-  constructor(private actividadService: ActividadesService) {}
+  constructor(
+    private actividadesService: ActividadesService,
+    private auditoriaactividadesService: AuditoriaActividadesService,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-//    this.loadActividades();
+  ngOnInit() {
+    this.columnas = [
+      { field: 'idActividad', header: 'Id' },
+      { field: 'descripcion', header: 'Descripción', filter: true },
+      { field: 'prioridad', header: 'Prioridad' },
+      //{ field: 'responsable', header: 'Responsable' },
+      { field: 'estado', header: 'Estado' },
+    ];
+
+    this.opcionesDeFiltro = [
+      {
+        value: 'startsWith',
+        label: 'Empieza con',
+      },
+      {
+        value: 'contains',
+        label: 'Contiene',
+      },
+    ];
+    this.llenarTabla();
   }
-/*
-  loadActividades() {
-    // this.actividadService.gettareasEjecutor().subscribe((data) => {
-    //   this.actividades = data;
-      console.log('Actividades del ejecutor:', this.actividades);
-    // });
+
+  llenarTabla() {
+    this.auditoriaactividadesService.getActividades().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.actividades = this.transformarDatos(res);
+      },
+      error: (err) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Ocurrió un error al recuperar la lista de actividades',
+        });
+      },
+    });
   }
-*/
-  eliminarActividad() {
-  
-  console.log('Eliminar Actividad');
-}
-  Finalizar() {
-    console.log('Finalizar');
+
+  transformarDatos(data: ActividadDto[]): ActividadDto[] {
+    return data.map(actividad => ({
+      ...actividad,
+      responsable: actividad.idUsuarioActual ? actividad.idUsuarioActual.nombreUsuario : ''
+    }));
+  }
+
+  eliminar() {
+    this.accion = 'Eliminar';
+    this.dialogVisible = true;
+  }
+
+  finalizar() {
+    this.accion = 'Finalizar';
+    this.dialogVisible = true;
   }
 }
