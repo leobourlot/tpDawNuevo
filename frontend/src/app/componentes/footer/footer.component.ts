2126aabc21acf633d1fbb82d64f4947626640a05
leobourlot <leobourlot@gmail.com>
2024-05-26 21:15:17 -0300

    header y footer completos

--- frontend/src/app/componentes/footer/footer.component.ts
+++ frontend/src/app/componentes/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
-
+import { BaseComponent } from "../base/base.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 @Component({
     selector: 'app-footer',
     standalone: true,
-    imports: [],
+    imports: [BaseComponent, FontAwesomeModule],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss',
 })
